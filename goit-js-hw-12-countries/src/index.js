76ae803676f7a23428768d218353ac5e5de9a531
Evgen Bielich <belichevgenij@gmail.com>
2021-05-26 20:18:47 +0300

    add index.js

--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -1,1 +1,9 @@
-import './sass/main.scss';
+import API from './js/fetchCountries.js';
+import getRefs from './js/refs.js';
+import countryInformation from './templates/country-information.hbs';
+import countriesList from './templates/countries-list.hbs';
+import debounce from 'lodash.debounce';
+import { error } from '@pnotify/core/dist/PNotify.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const refs = getRefs();
