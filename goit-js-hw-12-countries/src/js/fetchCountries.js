a8fb4ccc7126068bf9efd822d799a8e5820aac89
Evgen Bielich <belichevgenij@gmail.com>
2021-05-26 20:11:51 +0300

    add fetch.js

---
+++ goit-js-hw-12-countries/src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const baseUrl = 'https://restcountries.eu/rest/v2/name/';
+
+function fetchCountries(searchQuery) {
+  return fetch(baseUrl + `${searchQuery}`).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(console.error());
+  });
+}
+
+export default { fetchCountries };
