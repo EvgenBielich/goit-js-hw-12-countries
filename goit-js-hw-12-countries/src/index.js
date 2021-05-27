2f74015a311434ee5108baad9b2e90aec6ccb309
Evgen Bielich <belichevgenij@gmail.com>
2021-05-27 21:00:46 +0300

    add index.js

--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -7,3 +7,36 @@ import { error } from '@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = getRefs();
+
+refs.searchForm.addEventListener('input', debounce(getCountryInfo, 500));
+function getCountryInfo(e) {
+   e.preventDefault();
+   clearMarkup();
+   const searchQuery = e.target.value;
+   API.fetchCountries(searchQuery).then(renderCountryInfo).catch(handleFetchCountryError);
+}
+
+function renderCountryInfo(country) {
+   if (country.length === 1) {
+      const markup = countryInformation(country);
+      refs.articlesList.insertAdjacentHTML('beforeend', markup);   
+} else if (country.length >= 2 && country.length <= 10) {
+   const markup = countriesList(country);
+   refs.articlesList.insertAdjacentHTML('beforeend', markup);
+} else if (country.length > 10) {
+   error({
+      text: 'Too many matches found. Please enter a more specific query!',
+   });
+} else {
+   clearMarkup();
+   return;
+}
+};
+
+function handleFetchCountryError() {
+   console.log("Ops there is mistake!");
+}
+
+function clearMarkup() {
+   refs.articlesList.innerHTML = '';
+}
